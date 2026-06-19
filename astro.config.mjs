00917f8cc7e31e4d05e1fa48c9475bed311fd83f
shubham yadav <shubhamdv19@gmail.com>
2026-06-19 19:04:45 +0530

    Adjust default site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
-const site = process.env.SITE_URL ?? "https://shubhamyadav.dev";
+const site = process.env.SITE_URL ?? "";
 const base = process.env.BASE_PATH ?? "/";
 
 export default defineConfig({
