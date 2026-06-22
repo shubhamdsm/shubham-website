144fbc4c8fa86e746c26f8f29e5274643a8335ca
shubham yadav <shubhamdv19@gmail.com>
2026-06-22 11:58:11 +0530

    added local server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
-const site = process.env.SITE_URL ?? "";
+const site = process.env.SITE_URL ?? "http://localhost:4321";
 const base = process.env.BASE_PATH ?? "/";
 
 export default defineConfig({
